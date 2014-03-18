2d5532af6557fb9f9667629ca104132a6eb70362
Tom Zmyslo <tzmyslo25@gmail.com>
2014-03-18 00:09:16 -0400

    added Nashville gem, removed Turbolinks, added Results controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Playlist::Application.routes.draw do
+  get "results/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
